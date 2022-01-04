e5b34118e0d1a244382460e22c972bc755b81b6e
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-03 22:25:31 -0800

    app is functioning

--- src/components/PlayerDetail.js
+++ src/components/PlayerDetail.js
@@ -7,6 +7,9 @@ export default function PlayerDetail({ player }) {
       <div>
         <p>position: {player.position}</p>
         <p>jersey number: {player.id}</p>
+        <p>
+          team: <Link to={`/teams/${player.team_id}`}>{player.team_id}</Link>
+        </p>
       </div>
     </>
   );
